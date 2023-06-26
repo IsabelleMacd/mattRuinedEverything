78c814ac032dd70a0d24f27603d444223b4d3f96
Isabelle <isabellemacdonaldd@gmail.com>
2023-06-26 15:24:38 -0400

    Initial Commit

---
+++ mattRuinedEverything/mattRuinedEverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mattRuinedEverythingApp.swift
+//  mattRuinedEverything
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct mattRuinedEverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
